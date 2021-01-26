71d707b9c941d6fef35d932f8dff23b2938b4b28
Sean Belverstone <seanbelverstone@gmail.com>
2021-01-25 18:15:03 -0600

    Added animation to image and button when clicking. Realise though that I need a login section first, not just a play button so building a backend

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./assets/images/Antre.png";
 import "./App.css";
 
 function App () {
+
+    const [hide, setHide] = useState(false);
+
+    
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="logo" alt="logo" />
-                <button id="playButton" onClick={hideLogo}>Play</button>
+                <img src={logo} className={hide ? "hide" : "show"} alt="logo"/>
+                <button 
+                    id="playButton" 
+                    onClick={() => setHide(!hide)} 
+                    className={hide ? "hide" : "show"}>
+                        Play
+                </button>
             </header>
         </div>
     );
-}
-
-const hideLogo = () => {
-    console.log()
 }
 
 export default App;
