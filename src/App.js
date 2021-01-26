7444d1b67d6764cf49b90096bf859eca63cc0b6d
Sean Belverstone <seanbelverstone@gmail.com>
2021-01-25 19:31:51 -0600

    Moved the hide/show to the App-header instead, and changed it to an ID, so now when login is clicked the whole thing shrinks into nothingness

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App () {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className={hide ? "hide" : "show"} alt="logo" id="logo"/>
+            <header className="App-header" id={hide ? "hide" : "show"}>
+                <img src={logo} alt="logo" id="logo"/>
                 {/* <button 
                     id="playButton" 
                     onClick={() => setHide(!hide)} 
@@ -20,9 +20,13 @@ function App () {
                         Play
                 </button> */}
                 <form noValidate autoComplete="off" id="login">
-                    <TextField id="outlined-basic" label="Username" variant="outlined"/>
-                    <TextField id="outlined-basic" label="Password" variant="outlined" />
-                    <Button variant="contained" color="primary">
+                    <TextField className="formInput" label="Username" variant="outlined"/>
+                    <TextField className="formInput" label="Password" variant="outlined" type="password"/>
+                    <Button 
+                        variant="contained" 
+                        color="primary"
+                        onClick={() => setHide(!hide)} 
+                        >
                         Login
                     </Button>
                     <a id="create">CREATE AN ACCOUNT</a>
