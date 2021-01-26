3e16fe2ab4422ae860ff9d5e672ad904633c06b0
Sean Belverstone <seanbelverstone@gmail.com>
2021-01-25 19:15:42 -0600

    Added a form, login button and change the image slightly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import logo from "./assets/images/Antre.png";
+import logo from "./assets/images/AntreLarge.png";
+import { TextField, Button } from "@material-ui/core";
 import "./App.css";
 
 function App () {
@@ -11,13 +12,24 @@ function App () {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className={hide ? "hide" : "show"} alt="logo"/>
-                <button 
+                <img src={logo} className={hide ? "hide" : "show"} alt="logo" id="logo"/>
+                {/* <button 
                     id="playButton" 
                     onClick={() => setHide(!hide)} 
                     className={hide ? "hide" : "show"}>
                         Play
-                </button>
+                </button> */}
+                <form noValidate autoComplete="off" id="login">
+                    <TextField id="outlined-basic" label="Username" variant="outlined"/>
+                    <TextField id="outlined-basic" label="Password" variant="outlined" />
+                    <Button variant="contained" color="primary">
+                        Login
+                    </Button>
+                </form>
+                <div>
+                    <a id="create">CREATE AN ACCOUNT</a>
+                </div>
+                
             </header>
         </div>
     );
