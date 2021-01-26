ca78218e2a55d97ce78fdd56f87539cd6aecccdd
Sean Belverstone <seanbelverstone@gmail.com>
2021-01-25 21:51:53 -0600

    Installed express, added routes, models, controllers folder

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const router = express.Router();
+require("dotenv").config();
+import routes from "./routes";
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Middleware
+app.use(express.json())
+routes(app);
+
+
+// Send every request to the React app
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next()
+});
+
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
