432faae173d990ceaf3d06b97c861bf2aef5a117
Sean Belverstone <seanbelverstone@gmail.com>
2021-01-31 16:53:33 -0600

    Uncommented the auth route, as i`ll be using it for the login component.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,5 @@
 
 export default function(app) {
     app.route("/api/auth/")
-        // .post(authController.validate);
-        // Uncomment when other routes are created
+        .post(authController.validate);
 }
