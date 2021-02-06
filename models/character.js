d2086ffe819bf7b0fc263722d6e2651bf237b077
Sean Belverstone <seanbelverstone@gmail.com>
2021-02-06 12:32:54 -0600

    Changed the class piece in model to be CharClass to prevent errors on the front end. Still getting the error of changeToCharacter is not a function, i think i need a better solution to this.

--- models/character.js
+++ models/character.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        class: {
+        charClass: {
             type: DataTypes.STRING,
             allowNull: false,
         },
