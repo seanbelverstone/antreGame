d5f7952d9679956da20118e7c73f096f630f0814
Sean Belverstone <seanbelverstone@gmail.com>
2021-02-07 20:01:22 -0600

    Added a section to display stats for each class

--- client/src/components/CreateCharacter/index.js
+++ client/src/components/CreateCharacter/index.js
@@ -5,6 +5,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import { TextField, Button } from "@material-ui/core";
 import FormControl from '@material-ui/core/FormControl';
+import { navigate } from "hookrouter";
 import API from "../../utils/API";
 import smallLogo from "../../assets/images/Antre.png";
 import "./style.css";
@@ -32,8 +33,56 @@ const CreateCharacter = () => {
     const [defense, setDefense] = useState(0);
     const [wisdom, setWisdom] = useState(0);
     const [luck, setLuck] = useState(0);
+    const [description, setDescription] = useState("");
+    const [skill, setSkill] = useState("");
 
     const userId = window.sessionStorage.getItem("id");
+
+    // By passing in charClass, setStats runs whenever the selected class is selected
+    useEffect((charClass) => {
+        setStats();
+    })
+
+    // sets the stats of the corresponding class
+    const setStats = () => {
+        switch (charClass) {
+            case "Warrior":
+                setHealth(80)
+                setStrength(5);
+                setDefense(4);
+                setWisdom(1);
+                setLuck(2);
+                setDescription("Strong and fierce, the warrior is a reliable combatant.")
+                setSkill("Stalwart Defense: Impervious to damage for 1 turn.")
+                break;
+            case "Rogue":
+                setHealth(60)
+                setStrength(4);
+                setDefense(2);
+                setWisdom(2);
+                setLuck(4);
+                setDescription("As deadly as they are cunning, the rogue utilizes their luck to end fights quickly.")
+                setSkill("Rapid Attack: Doubles special attack damage")
+                break;
+            case "Paladin":
+                setHealth(70)
+                setStrength(3);
+                setDefense(3);
+                setWisdom(4);
+                setLuck(2);
+                setDescription("Using other worldly powers to surpass their foes, the paladin is the wisest of them all.")
+                setSkill("Holy Remedy: Restores 30% health")
+                break;
+            default:
+                setHealth(0)
+                setStrength(0);
+                setDefense(0);
+                setWisdom(0);
+                setLuck(0);
+                setDescription("")
+                setSkill("")
+        }
+    }
 
     const handleRaceChange = (event) => {
         setRace(event.target.value);
@@ -56,34 +105,8 @@ const CreateCharacter = () => {
         } else {
             setNameError(false);
             setNameHelperText("");
-            setStats();
+            createNewCharacter();
         }
-    }
-
-    const setStats = () => {
-        switch (charClass) {
-            case "warrior":
-                setHealth(80)
-                setStrength(5);
-                setDefense(4);
-                setWisdom(1);
-                setLuck(2);
-                break;
-            case "rogue":
-                setHealth(60)
-                setStrength(3);
-                setDefense(2);
-                setWisdom(2);
-                setLuck(5);
-                break;
-            default:
-                setHealth(70)
-                setStrength(3);
-                setDefense(3);
-                setWisdom(4);
-                setLuck(2);
-        }
-        createNewCharacter();
     }
 
     const createNewCharacter = () => {
@@ -138,11 +161,23 @@ const CreateCharacter = () => {
                             value={charClass}
                             onChange={handleClassChange}
                             >
-                            <MenuItem value={"Warrior"}>Warrior</MenuItem>
-                            <MenuItem value={"Rogue"}>Rogue</MenuItem>
-                            <MenuItem value={"Paladin"}>Paladin</MenuItem>
+                            <MenuItem value={"Warrior"} id="warrior">Warrior</MenuItem>
+                            <MenuItem value={"Rogue"} id="rogue">Rogue</MenuItem>
+                            <MenuItem value={"Paladin"} id="paladin">Paladin</MenuItem>
                         </Select>
                     </FormControl>
+                    <div id="classDescription">
+                        <h3>{charClass}</h3>
+                        <div id="stats">
+                            <div>HP: {health}</div>
+                            <div>Strength: {strength}</div>
+                            <div>Defense: {defense}</div>
+                            <div>Wisdom: {wisdom}</div>
+                            <div>Luck: {luck}</div>
+                        </div>
+                        <div id="description">{description}</div>
+                        <div id="skill">{skill}</div>
+                    </div>
                     <Button 
                         variant="contained" 
                         color="primary"
