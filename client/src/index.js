6d261c902d7f72c07802e7ae0af4ec4a01ab6b3b
Sean Belverstone <seanbelverstone@gmail.com>
2021-03-27 10:02:11 -0500

    Forgot to add the import for it in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from './serviceWorker';
 import "./index.css";
 
 render(
