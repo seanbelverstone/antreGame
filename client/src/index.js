540ef2ab3ae5afc2ee431ce82128805a511284b0
Sean Belverstone <seanbelverstone@gmail.com>
2021-03-27 09:58:31 -0500

    added service worker and linked it to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -13,3 +13,5 @@ render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// Enables the app to work offline and loads faster
+serviceWorker.register();
