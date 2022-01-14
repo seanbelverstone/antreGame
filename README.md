e7abe88c7f81cf108623ea8d638ecf000656ab05
Sean Belverstone <seanbelverstone@gmail.com>
2022-01-14 15:48:48 -0600

    Gutted out the server, and moved everything into the root folder. Added an endpoint variable which will communicate with my live server. Running into cors issues atm though

--- README.md
+++
@@ -1,9 +0,0 @@
-# ANTRE
-A turn based, text adventure game run in Javascript using React &amp; Redux.
-
-Technologies
-- React
-- Redux
-- Javascript
-- Node
-- Material-UI
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+const endpoint = 'https://antre-game-server.herokuapp.com'
 
 export default {
 
     createUser: (username, email, password) => {
-        return axios.post("/api/users", {
+        return axios.post(`${endpoint}/api/users`, {
             username, 
             email, 
             password
@@ -11,21 +12,21 @@ export default {
     },
 
     checkUser: (username, password) => {
-        return axios.post("/api/auth", {
+        return axios.post(`${endpoint}/api/auth`, {
             username, 
             password
         }) 
     },
 
     getAllCharacters: (userId, jwtToken) => {
-        return axios.get(`/api/characters/${userId}`, {
+        return axios.get(`${endpoint}/api/characters/${userId}`, {
             headers: {
                 'Authorization': `Bearer ${jwtToken}`}
         })
     },
 
     createNewCharacter: (name, race, charClass, health, strength, defense, wisdom, luck, UserId, jwtToken) => {
-        return axios.post("/api/characters", {
+        return axios.post(`${endpoint}/api/characters`, {
             name, 
             race, 
             charClass,
@@ -44,7 +45,7 @@ export default {
     saveCharacter: (stats, inventory, levels, time, jwtToken) => {
         const { id, health, strength, defense, wisdom, luck } = stats;
         const {weapon, weaponDamage, head,  chest,  legs,  hands,  feet,  torch,  amulet,  healthPotions,  gold } = inventory;
-        return axios.put(`/api/characters/${id}`, {
+        return axios.put(`${endpoint}/api/characters/${id}`, {
             id,
             health,
             strength,
@@ -71,7 +72,7 @@ export default {
     },
     
     deleteCharacter: (characterId, jwtToken) => {
-        return axios.delete(`/api/characters/${characterId}`, {
+        return axios.delete(`${endpoint}/api/characters/${characterId}`, {
             headers: {
                 'Authorization': `Bearer ${jwtToken}`}
         })
