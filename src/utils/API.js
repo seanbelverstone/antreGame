117e70857a25db20ad66b26d54505a38eb8c0b26
Sean Belverstone <seanbelverstone@gmail.com>
2022-01-17 17:38:01 -0600

    sending console so i can figure out why we're not communicating to the BE properly

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import axios from 'axios';
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 const endpoint = env === 'development' ? 'http://localhost:3001' : 'https://antre-game-server.herokuapp.com';
 
 export default {
